4356bc0327cbb4c9c71daeadb3af749886565ca6
joshuapalma <joshuapalma57@gmail.com>
2022-10-18 02:14:16 +0800

    commit revert route

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,9 @@ use App\Http\Controllers\ChangePassword;
 use App\Http\Controllers\MainController;
 
 
-Route::get('/', [MainController::class, 'index'])->middleware('guest')->name('main');
+// Route::get('/home', [MainController::class, 'index'])->middleware('guest')->name('main');
 
-Route::get('/admin', function () {return redirect('/dashboard');})->middleware('auth');
+Route::get('/', function () {return redirect('/dashboard');})->middleware('auth');
 	Route::get('/register', [RegisterController::class, 'create'])->middleware('guest')->name('register');
 	Route::post('/register', [RegisterController::class, 'store'])->middleware('guest')->name('register.perform');
 	Route::get('/login', [LoginController::class, 'show'])->middleware('guest')->name('login');
