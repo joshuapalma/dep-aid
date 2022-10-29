35170be067a0b327a48255c2f0634a67a959c810
joshuapalma <joshuapalma57@gmail.com>
2022-10-29 15:51:53 +0800

    commit UI

--- resources/views/pages/anti-inflammatory.blade.php
+++ resources/views/pages/anti-inflammatory.blade.php
@@ -3,18 +3,103 @@
 @section('content')
     @include('layouts.navbars.auth.topnav', ['title' => 'Inventory / Anti-Inflammatory'])
     <div class="container-fluid py-4">
-        <div class="row">
+        <div class="row mt-4">
             <div class="col-lg-12 mb-lg-0 mb-4">
                 <div class="card z-index-2 h-100" style="background-color: transparent; border: none; box-shadow: none;">
-                    <div class="col-lg-12 col-md-12 d-flex justify-content-center">
-                        <a href="#" class="btn btn-secondary z-index-2 me-2">Cardiac Drugs</a>
-                        <a href="#" class="btn btn-secondary z-index-2 me-2">Antibiotics</a>
-                        <a href="#" class="btn btn-secondary z-index-2 me-2 active">Anti-inflammatory</a>
-                        <a href="#" class="btn btn-secondary z-index-2 me-2">Ear Meds</a>
-                        <a href="#" class="btn btn-secondary z-index-2 me-2">Topicals</a>
+                    <div class="col-lg-12 col-md-12 d-flex justify-content-end">
+                        <button class="btn bg-gradient-info z-index-2 me-2" data-bs-toggle="modal" data-bs-target="#filterLeaveModal">Filter</button>
+                        <button class="btn bg-gradient-info z-index-2 me-2" data-bs-toggle="modal" data-bs-target="#exportLeaveModal">Generate Report</button>
+                        <button type="button" class="btn bg-gradient-success z-index-2" data-bs-toggle="modal" data-bs-target="#addLeaveModal">Add Anti-Inflammatory</button>
                     </div>
                 </div>
-            </div>  
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-6 col-md-6">
+                <form action="{{route('anti-inflammatory.index')}}" method="GET">
+                    <div class="form-group">
+                        <div class="input-group">
+                            {{-- <input class="form-control" type="text" placeholder="Search.." name="search" value="{{ $requestData['search'] }}"> --}}
+                            <input class="form-control" type="text" placeholder="Search.." name="search" value="">
+                            <button class="search-btn" type="submit" style="border: none; border-top-right-radius: 10px; border-bottom-right-radius: 10px; backgropund-color: #ededed;"><i class="ni ni-zoom-split-in" style="padding-left: 5px; padding-right: 5px"></i></button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-lg-12 mb-lg-0 mb-4">
+                <div class="card z-index-2 h-100">
+                    <div class="card-body p-3">
+                        <div class="table-responsive">
+                            <table class="table align-items-center mb-0">
+                              <thead>
+                                <tr>
+                                  <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 table-text">ID</th>
+                                  <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 table-text ps-2">Medicine Name</th>
+                                  <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 table-text ps-2">Brand</th>
+                                  <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 table-text">Manufacturer Date</th>
+                                  <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 table-text">Expiration Date</th>
+                                  <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 table-text">Action</th>
+                                </tr>
+                              </thead>
+                              <tbody>
+                                {{-- @forelse ($leave as $index => $row)
+                                    <tr class="text-center">
+                                        <td>
+                                            <p class="text-xs font-weight-bold table-text mb-0">{{ $index + 1 }}</p>
+                                        </td>
+                                        <td>
+                                            <p class="text-xs font-weight-bold table-text mb-0">{{ ucfirst($row->name) }}</p>
+                                        </td>
+                                        <td class="align-middle text-center text-sm">
+                                            <span class="text-secondary text-xs font-weight-bold table-text">{{ ucfirst($row->designation) }}</span>
+                                        </td>
+                                        <td class="align-middle text-center">
+                                            <span class="text-secondary text-xs font-weight-bold table-text">{{ date('m/d/Y', strtotime($row->date_of_leave)) }}</span>
+                                        </td>
+                                        <td class="align-middle text-center">
+                                            <span class="text-secondary text-xs font-weight-bold table-text">{{ getNatureOfLeave()[$row->nature_of_leave] }} {{ $row->specify_others ? '/ '.($row->specify_others) : "" }}</span>
+                                        </td>
+                                        <td class="align-middle">
+                                            <input type="hidden" id="leave-details-{{$row->id}}" data-detail="{{ $row }}">
+                                            <button 
+                                                type="button" 
+                                                class="btn bg-gradient-warning z-index-2" 
+                                                data-bs-toggle="modal" 
+                                                data-bs-target="#editLeaveModal" 
+                                                onclick = "editLeave('{{$row->id}}')">
+                                                Edit
+                                            </button>
+                                            <button 
+                                                type="button" 
+                                                class="btn bg-gradient-danger z-index-2 drop" 
+                                                data-bs-toggle="modal" 
+                                                data-bs-target="#deleteModal"
+                                                data-url="{{ route('leave.destroy', $row->id) }}"
+                                                onclick = "deleteLeave(this)">
+                                                Delete
+                                            </button>
+                                        </td>
+                                    </tr>
+                                @empty --}}
+                                    <tr>
+                                        <td colspan="6" class="font-weight-bold text-center table-text">No Data Available</td>
+                                    </tr>
+                                {{-- @endforelse --}}
+                              </tbody>
+                            </table>
+                        </div>
+                        {{-- <div class="table-pagination p-5">
+                            <div class="row">
+                                <div class="row col-sm-12 col-md-12 col-lg-12 font-weight-600"">
+                                    {{$leave->appends(['search' => isset($requestData->search) ? $requestData->search : null])->links('components.pagination')}}
+                                </div>
+                            </div>
+                        </div> --}}
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -4,7 +4,7 @@
     @include('layouts.navbars.auth.topnav', ['title' => 'Dashboard'])
     <div class="container-fluid py-4">
         <div class="row">
-            <div class="col-xl-4 col-sm-6 mb-xl-0 mb-4">
+            <div class="col-xl-6 col-sm-6 mb-xl-0 mb-4">
                 <div class="card">
                     <div class="card-body p-3">
                         <div class="row">
@@ -25,36 +25,15 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xl-4 col-sm-6 mb-xl-0 mb-4">
+            <div class="col-xl-6 col-sm-6 mb-xl-0 mb-4">
                 <div class="card">
                     <div class="card-body p-3">
                         <div class="row">
                             <div class="col-8">
                                 <div class="numbers">
-                                    <p class="text-sm mb-0 text-uppercase font-weight-bold">Inventory</p>
+                                    <p class="text-sm mb-0 text-uppercase font-weight-bold">Scheduled Patients</p>
                                     <h5 class="font-weight-bolder">
-                                        2,300
-                                    </h5>
-                                </div>
-                            </div>
-                            <div class="col-4 text-end">
-                                <div class="icon icon-shape bg-gradient-danger shadow-danger text-center rounded-circle">
-                                    <i class="fa-solid fa-warehouse text-lg opacity-10" aria-hidden="true"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xl-4 col-sm-6 mb-xl-0 mb-4">
-                <div class="card">
-                    <div class="card-body p-3">
-                        <div class="row">
-                            <div class="col-8">
-                                <div class="numbers">
-                                    <p class="text-sm mb-0 text-uppercase font-weight-bold">Waiting Patients</p>
-                                    <h5 class="font-weight-bolder">
-                                        +3,462
+                                        3,462
                                     </h5>
                                 </div>
                             </div>
@@ -68,6 +47,129 @@
                 </div>
             </div>
         </div>
+        <div class="row mt-3 mb-3">
+            <div class="col-xl-12 col-md-12 col-sm-6 mb-xl-0 mb-4">
+                <div class="card">
+                    <div class="card-body p-3">
+                        <div class="row">
+                            <h6 class="font-weight-bolder text-uppercase">Inventory</h6>
+                        </div>
+
+                        <div class="row mt-3 mb-3">
+                            <div class="col-xl-4 col-sm-6 mb-xl-0 mb-4">
+                                <div class="card">
+                                    <div class="card-body p-3">
+                                        <div class="row">
+                                            <div class="col-8">
+                                                <div class="numbers">
+                                                    <p class="text-sm mb-0 text-uppercase font-weight-bold">Cardiac Drugs Count</p>
+                                                    <h5 class="font-weight-bolder">
+                                                        100
+                                                    </h5>
+                                                </div>
+                                            </div>
+                                            <div class="col-4 text-end">
+                                                <div class="icon icon-shape bg-gradient-primary shadow-primary text-center rounded-circle">
+                                                    <i class="fa-solid fa-hospital-user text-lg opacity-10" aria-hidden="true"></i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-4 col-sm-6 mb-xl-0 mb-4">
+                                <div class="card">
+                                    <div class="card-body p-3">
+                                        <div class="row">
+                                            <div class="col-8">
+                                                <div class="numbers">
+                                                    <p class="text-sm mb-0 text-uppercase font-weight-bold">Antibiotics Count</p>
+                                                    <h5 class="font-weight-bolder">
+                                                        100
+                                                    </h5>
+                                                </div>
+                                            </div>
+                                            <div class="col-4 text-end">
+                                                <div class="icon icon-shape bg-gradient-primary shadow-primary text-center rounded-circle">
+                                                    <i class="fa-solid fa-hospital-user text-lg opacity-10" aria-hidden="true"></i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-4 col-sm-6 mb-xl-0 mb-4">
+                                <div class="card">
+                                    <div class="card-body p-3">
+                                        <div class="row">
+                                            <div class="col-8">
+                                                <div class="numbers">
+                                                    <p class="text-sm mb-0 text-uppercase font-weight-bold">Ear Meds Count</p>
+                                                    <h5 class="font-weight-bolder">
+                                                        100
+                                                    </h5>
+                                                </div>
+                                            </div>
+                                            <div class="col-4 text-end">
+                                                <div class="icon icon-shape bg-gradient-primary shadow-primary text-center rounded-circle">
+                                                    <i class="fa-solid fa-hospital-user text-lg opacity-10" aria-hidden="true"></i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-xl-6 col-sm-6 mb-xl-0 mb-4">
+                                <div class="card">
+                                    <div class="card-body p-3">
+                                        <div class="row">
+                                            <div class="col-8">
+                                                <div class="numbers">
+                                                    <p class="text-sm mb-0 text-uppercase font-weight-bold">Topicals Count</p>
+                                                    <h5 class="font-weight-bolder">
+                                                        100
+                                                    </h5>
+                                                </div>
+                                            </div>
+                                            <div class="col-4 text-end">
+                                                <div class="icon icon-shape bg-gradient-primary shadow-primary text-center rounded-circle">
+                                                    <i class="fa-solid fa-hospital-user text-lg opacity-10" aria-hidden="true"></i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-6 col-sm-6 mb-xl-0 mb-4">
+                                <div class="card">
+                                    <div class="card-body p-3">
+                                        <div class="row">
+                                            <div class="col-8">
+                                                <div class="numbers">
+                                                    <p class="text-sm mb-0 text-uppercase font-weight-bold">Anti-Inflammatory Count</p>
+                                                    <h5 class="font-weight-bolder">
+                                                        100
+                                                    </h5>
+                                                </div>
+                                            </div>
+                                            <div class="col-4 text-end">
+                                                <div class="icon icon-shape bg-gradient-primary shadow-primary text-center rounded-circle">
+                                                    <i class="fa-solid fa-hospital-user text-lg opacity-10" aria-hidden="true"></i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <div class="row">
             
         </div>
