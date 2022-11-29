f5f6834523d1d8eb1c6f2d25f4f49c191f05098a
JoshuaPalma <joshuapalma57@gmail.com>
2022-11-29 15:40:06 +0800

    commit fix images

--- resources/views/pages/patient-form.blade.php
+++ resources/views/pages/patient-form.blade.php
@@ -637,7 +637,7 @@
                       <div
                           class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
                           <div class="position-relative h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden"
-                              style="background-image: url('img/medical.svg');
+                              style="background-image: url('/public/img/medical.svg');
             background-size: cover;">
                               <span class="mask opacity-6"></span>
                           </div>
